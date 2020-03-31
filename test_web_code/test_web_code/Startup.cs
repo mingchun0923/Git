7c65835fcba159f98318a8e8041437ae9f01a900
piaomc <mingchun.piao@hotmail.com>
2020-03-31 16:04:05 +0800

    new code

---
+++ test_web_code/test_web_code/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_web_code.Startup))]
+namespace test_web_code
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
